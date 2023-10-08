28ce1a56428b5ccd5f221f19b96e0c1c880d442d
MaximKa1111 <147326050+MaximKa1111@users.noreply.github.com>
2023-10-08 18:25:35 +0300

    Update code.py

--- lesson2/code.py
+++ lesson2/code.py
@@ -50,3 +50,50 @@ import colorama
 #   return None
 # print(find_index([1,2,3,4],6))
 # def replace(l,k,n):
+# import random
+# def find_elms(l):
+#   result_array=[]
+# for i in range(len(l)):
+#   if i%2==1 and l[i]%2==0:
+#      result_array.append(l[i])
+#    return_result_array
+# l=[random.randint(1,1000) for i in range(100)]
+# res=find_elems(l)
+# print(res)
+# message= 'hellow world1'
+# f=open('test.txt','w')
+# f.write(message)
+# f.close()
+
+# with open('test.txt','w') as f:
+#   f.write(message)
+
+# with open('test.txt','r') as f:
+#   message=f.read()
+#   print(message)
+
+# arr=[]
+# with open('test.txt','w') as f:
+#   for i in f:
+#     arr .append(int(i))
+# print(arr)
+
+# import random
+# def random_text(l):
+#   text=[]
+# l=[random.randit9(1,100) for i in range (100)]
+# text.append(l)
+# with open('test.txt','w') as f:
+# f.writen(text)
+
+# import random
+# def write_to_file(arr,path_to_file):
+#   with open(path_to_file,'a+') as f:
+#    for i in arr:
+#      f.write(str(i)+'\n')
+# PATH_TO_FILE='test.txt'
+# write_to_file([random.randint(1,1000) for i in range(100)], PATH_TO_FILE)
+
+
+  
+
