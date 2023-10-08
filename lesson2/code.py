4b7dfe557c57fe71c4c0b2149d5adcda57adfde5
MaximKa1111 <147326050+MaximKa1111@users.noreply.github.com>
2023-10-08 17:21:40 +0300

    Create code.py

---
+++ lesson2/code.py
@@ -0,0 +1,52 @@
+# aboutMax={'name':'Maxim','age':39, 'profession':'engineer', 'garde':'[1,2,3,4,5]'}
+# print(aboutMax['grade'])
+# black_area='b'
+# white_area='w'
+# chess_field=[]
+# for i in range(8):
+#   middleware_array=[]
+#   for j in range(8):
+#     if (i + j) % 2 == 0:
+#       middleware_array.append('b')
+#     else:
+#       middleware_array.append('w')
+#       chess_field.append(middleware_array)
+# print(chess_field)
+# def hello_name(name):
+#   print('Hello, '+str(name))
+# import random
+import colorama
+# def calculator(value1,value2,operation_type="+"):
+#   if operation_type=='+':
+#     return value1 + value2
+#   elif operation_type=='-':
+#     return value1 - value2
+# result1=calculator(1,2)
+# result2=calculator(10,8,'-')
+# print (result2)
+# print(colorama.Fore.BLUE,'Hello Max')
+# from lesson2.functions import my_print
+# my_print
+# import random 
+# def generate_password(length,register=False,special=False):
+#   chars='wqqfjuweiofljegve'
+#   register_chars='QWEQWEWER'
+#   special_chars='!@#$%^&'
+#   password=''
+#   if register:
+#     chars+=register_chars
+#   if special:
+#     chars+=special_chars
+#   for i in range(length):
+#     password+=random.choice(chars)
+#   return password
+# password=generate_password(16,special=True,register=True)
+# print(password)
+# def find_index(l,k):
+#   for i in range(len(l)-1):
+#     for j in range(len(l)):
+#       if l[i]+l[j] == k:
+#         return i,j
+#   return None
+# print(find_index([1,2,3,4],6))
+# def replace(l,k,n):
